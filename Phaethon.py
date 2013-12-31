3c41efa1c9cf5b5a42b66d3221b9a7743532dea8
-v <sujeath@ras.beamtele.net>
2013-12-31 21:32:23 +0530

    merge

--- Phaethon.py
+++ Phaethon.py
@@ -10,14 +10,74 @@ def triggerNavalBattle(navies):
 def triggerArmyBattle(armies):
 		pass
 				
-class Faction:#A faction represents a side in the war, due to asymmetries in the faction game mechanics,
-			  #it would be better to inherit different sides in a hierarchy 
+class Faction:  
 		def __init__(self,capital,cities):
-				self.cities=cities
-				self.capital=capital
+        		self.capital = capital 
+                self.cities = [capital]+cities
+				
+		def getManPower(self):
+				return sum(map(lambda city:city.manPower,self.cities))
+				
+		def recruit(self,number):
+				mp=self.getManPower()
+				fractions=map(lambda x:x.manPower/mp,self.cities)
+				for i in range(len(self.cities)):
+						self.cities[i].manPower-=fraction[i]*self.cities[i].manPower
 
-class SuperFaction(Faction):#Roman, Hannibal or Carthage
-		pass
+		def getTribute(self):
+				pass
+				
+		def destroy(self):
+				pass
+		
+        def changeCapital(capital):
+				self.capital = capital
+
+class City:
+		 def __init__(self,province,alleigance,geoX,geoY,manPower=0,money=0):
+				self.manPower=manPower
+				self.money=money            
+				self.province=province
+				self.geoX=geoX
+				self.geoY=geoY
+				self.alleigance=alleigance
+				self.destroyed=False
+				self.beseiged=False
+
+         def besiege(self):
+                self.besieged=True
+				self.money*=0.33
+				self.manPower*=0.33
+				self.supply*=0.33
+		
+		def desiege(self):
+				self.besieged=False
+				pass
+
+        def destroy(self):
+                self.destroyed=True
+                self.money=0
+                self.manPower=0
+        
+        def sack(self):
+        		pass            
+
+class superFaction(Faction):
+         def __init__(self,allies,enemies):
+				self.allies=[]
+				self.enemies=[]
+				self.armies=[]
+				self.navies=[]
+
+class Province:
+		def __init__(self):
+            	pass 
+                
+                        
+                        
+
+                
+
 				
 class Route:
 		def __init__(self,points,A,B,provinces):#Provinces is a list of provinces that the route passes through
@@ -66,9 +126,4 @@ class GraphAnalyser:
 			def unblockedFrom(recrutingSuperFaction,recruitingRegion):
 					G=nx.MultiGraph()
 					G.add_edges_from([(route.A,route.B) for route in self.routes if not route.isBlockedFor(recruitingSuperFaction)])
-					return any(G.has_path(recruitingSuperFaction.capital,region) for region in recruitingRegion.cities)
-					
-			
-					
-					
-					
+					return any(G.has_path(recruitingSuperFaction.capital,region) for region in recruitingRegion.cities)				
