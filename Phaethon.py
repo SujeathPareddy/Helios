c6f1357a05ba2854978b798099b34aec9b2d1dde
-v <sujeath@ras.beamtele.net>
2013-12-31 20:13:00 +0530

    Ne Bondha

--- Phaethon.py
+++ Phaethon.py
@@ -1,47 +1,51 @@
-class Faction:
-                manPower = 0
-                money= 0
-                supply = 0
-                
-                def __init__(self,capital,cities):
-                        self.capital = capital 
-                        self.cities = cities
-                        for i in capital+cities :
-                            self.manPower += i.manPower
-                            self.money += i.money
-                            self.supply += supply
-    
-                def getManPower(self):
-                        return self.manPower
+class Faction:  
+		def __init__(self,capital,cities):
+        		self.capital = capital 
+                self.cities = [capital]+cities
+				
+		def getManPower(self):
+				return sum(map(lambda city:city.manPower,self.cities))
+				
+		def recruit(self,number):
+				mp=getManPower()
+				fractions=map(lambda x:x.manPower/mp,self.cities)
+				for i in range(len(self.cities)):
+						self.cities[i].manPower-=fraction[i]*self.cities[i].manPower
 
-                def getMoney (self):
-                        return self.money
-
-                
+		def getTribute(self):
+				pass
+				
+		def destroy(self):
+				pass
 
 class City:
+		 def __init__(self,province,alleigance,geoX,geoY):
+				self.manPower=0
+				self.money=0            
+				self.province=province
+				self.geoX=geoX
+				self.geoY=geoY
+				self.alleigance=alleigance
+				self.destroyed=False
+				self.beseiged=False
 
-                manPower = 0
-                money = 0
-                supply = 0
-                destroyed = False
-                beseiged = False
-                
+         def besiege(self):
+                self.besieged=True
+				self.money*=0.33
+				self.manPower*=0.33
+				self.supply*=0.33
+		
+		def desiege(self):
+				self.besieged=False
+				pass
 
-                def __init__(self,province,alleigance,geoX,geoY):
-                        self.province = province
-                        self.geoX = geoX
-                        self.geoY = geoY
-                        self.alleigance = alleigance
-
-                def besiege(self):
-                        self.besieged = True
-#                        self.money *= 0.33
-#                        self.manPower *= 0.33
-#                        self.supply *= 0.33
-
-                def destroy(self):
-                        self.destroyed = True               
+        def destroy(self):
+                self.destroyed=True
+                self.money=0
+                self.manPower=0
+        
+        def sack(self):
+        		pass            
 
 class superFaction(Faction):
 
